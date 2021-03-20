039febadaacaacd1cff7406348c2c8ff2a633eaf
Alexey Malyshev <leha3521@gmail.com>
2021-03-20 02:08:31 +0200

    Ready async audio recorder with .NET

---
+++ ASR proto/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Accord.Audio;
+using Accord.DirectSound;
+using Accord.Audio.Formats;
+
+namespace ASR_proto
+{
+    public class Recorder
+    {
+        private FileStream _fileStream;
+        private AudioCaptureDevice _microphone;
+        private WaveEncoder _audioSaver;
+        public Action FinishRecord { get; set; }
+        public string RecordPath { get; set; }
+
+        public Recorder(Action _finishRecord, string _path = "1.wav")
+        {
+            FinishRecord = _finishRecord;
+            RecordPath = _path;
+        }
+
+        private void source_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Read current frame...
+            Signal s = eventArgs.Signal;
+            // encode frame to file
+            _audioSaver.Encode(s);
+        }
+
+        public void SetRecorder(Guid? guid = null)
+        {
+            Guid? deviceGUID = guid;
+            var capture = new AudioDeviceCollection(AudioDeviceCategory.Capture);
+            foreach (var d in capture)
+            {
+                //Console.WriteLine($"{d.Guid}: {d.Description}");
+                if (d.Description.IndexOf("Audio") != -1)
+                {
+                    deviceGUID = d.Guid;
+                    break;
+                }
+            }
+            Guid _guid = deviceGUID ?? throw new Exception("Guid of capture devise is not set");
+            _microphone = new AudioCaptureDevice(_guid);
+            _microphone.SampleRate = 44100;
+            _microphone.Channels = 2;
+            _microphone.Format = SampleFormat.Format32BitIeeeFloat;
+            _microphone.NewFrame += source_NewFrame;
+        }
+
+        private async void OnListeningAsync()
+        {
+            Console.WriteLine("Press key to stop this shit!");
+            // Wait until operations end
+            var finishEventTask = new Task(FinishRecord);
+            finishEventTask.Start();
+            await finishEventTask;
+            Thread.Sleep(500); // For capturing last signals
+            _microphone.SignalToStop();
+        }
+
+        public async void RecordToAsync(string _path = "1.wav")
+        {
+            if (_microphone == null) SetRecorder();
+            if (FinishRecord == null) throw new Exception("FinishRecord function not set!");
+            if (_path == null) throw new ArgumentNullException("Error! Argument path is null!");
+            else if (_path != "1.wav") RecordPath = _path;
+
+
+            Task t = new Task(OnListeningAsync);
+            try
+            {
+                _fileStream = new FileStream(RecordPath, FileMode.OpenOrCreate, FileAccess.Write);
+                _audioSaver = new WaveEncoder();
+                _audioSaver.Open(_fileStream);
+                _microphone.Start();
+                t.Start();
+                await t;
+                _microphone.WaitForStop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                _audioSaver?.Close();
+                _fileStream?.Close();
+                t.Dispose();
+            }
+        }
+    }
+}
---
+++ ASR proto/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Accord.Audio;
+using Accord.DirectSound;
+using Accord.Audio.Formats;
+
+namespace ASR_proto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Recorder recorder = new Recorder(() => Console.ReadKey(), "3.wav");
+                recorder.RecordToAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+        }
+
+       /*
+        * Тезисы:
+        * Вступление.
+        * 
+        * Був розроблений проект застусунк на ЯП.
+        * 
+        * Цель которого реализовать многопоточноый
+        * обмен данными.
+        * 
+        * Распознавание фиксированных фраз.
+        * 
+        * Какие авторы это реализовали, какие были варианты.
+        * 
+        * Библиотеки на которых будет реализация.
+        * 
+        * Дальнейшая реализация проекта. Где может быть
+        * использована: школы, ВУЗы, музей науки.
+        * 
+        * актуально во время ковида, потому что безконтактное
+        * взаимодействие с компьютером.
+        * 
+        * Рассказать про нейронную сеть и схему нейронной сети.
+        * 
+        * Подать до 25.03
+        */
+
+
+    }
+}
