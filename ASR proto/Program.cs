b08ff2c63b85801b7da853caa405544dafa81065
Alexey Malyshev <leha3521@gmail.com>
2021-05-06 01:17:36 +0300

    Reading data from tsv files

--- ASR proto/Program.cs
+++ ASR proto/Program.cs
@@ -1,29 +1,73 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using Spectrogram;
+using CsvHelper;
+using CsvHelper.Expressions;
+using CsvHelper.TypeConversion;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 namespace ASR_proto
 {
     class Program
     {
+        [Serializable]
+        public class TrainData
+        {
+            public string client_id { get; set; }
+            public string path { get; set; }
+            public string sentence { get; set; }
+            public int up_votes { get; set; }
+            public int down_votes { get; set; }
+            public string age { get; set; }
+            public string gender { get; set; }
+            public string accent { get; set; }
+            public string locale { get; set; }
+            public string segment { get; set; }
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                string path = "3.wav";
-                Recorder recorder = new Recorder(null, path);
-                recorder.FinishRecord = () =>
+
+                //string path = "3.wav";
+                //Recorder recorder = new Recorder(null, path);
+                //recorder.FinishRecord = () =>
+                //{
+                //    Console.ReadKey();
+                //};
+                //recorder.RecordAudioToFile();
+                //while (recorder.IsRecordContinue) { }
+                //SpectrogramBuilder spectrogramBuilder = new SpectrogramBuilder();
+                //spectrogramBuilder.ImagePath = "images\\viridis.jpg";
+                //spectrogramBuilder.ImageColormap = Colormap.Viridis;
+                //spectrogramBuilder.BuildSpectrogram(path);
+
+                string path = @"D:\ML\Speech recognition\NLP_diploma\uk";
+                path = Path.Combine(path, "train.tsv");
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                List<TrainData> trainData = new List<TrainData>();
+
+                using (var _reader = new StreamReader(path))
                 {
-                    Console.ReadKey();
-                };
-                recorder.RecordAudioToFile();
 
-                while (recorder.IsRecordContinue) { }
-
-                SpectrogramBuilder spectrogramBuilder = new SpectrogramBuilder();
-                spectrogramBuilder.ImagePath = "images\\viridis.jpg";
-                spectrogramBuilder.ImageColormap = Colormap.Viridis;
-                spectrogramBuilder.BuildSpectrogram(path);
-
+                    CsvConfiguration myConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+                    myConfig.Delimiter = "\t";
+                    myConfig.Encoding = System.Text.Encoding.UTF8;
+                    using (var csvReader = new CsvReader(_reader, myConfig))
+                    {
+                        while (csvReader.Read())
+                        { 
+                            var data = csvReader.GetRecord<TrainData>();
+                            trainData.Add(data);
+                            
+                            Console.WriteLine($"{data.sentence}");
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -32,3 +76,4 @@ namespace ASR_proto
         }
     }
 }
+
